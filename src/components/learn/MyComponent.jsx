38e72f0054611d955127e987a55cb8a99b6a9c6f
unknown <masterrio2412@gmail.com>
2024-12-16 14:28:05 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,5 +1,17 @@
+import "./style.css";
+
 const MyComponent = () => {
-  return console.log("Hello World");
+  return (
+    <>
+      <div className="Name">Phat dep trai </div>
+      <div className="Child">Phat sieu dep trai</div>
+    </>
+  );
 };
 
 export default MyComponent;
+
+// jsx
+// chỉ được một phần tử html trong component
+// fragments: sẽ chứa các phần tử html trong component
+// khong render thua cac layout
