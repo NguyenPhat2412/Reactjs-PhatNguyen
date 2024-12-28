d17dd6a99ac4dd4c40f7d42a56cc4183a60705c4
unknown <masterrio2412@gmail.com>
2024-12-29 02:02:16 +0700

    video 35

--- src/components/todo/todo-input.jsx
+++ src/components/todo/todo-input.jsx
@@ -2,10 +2,23 @@ const TodoItem = (props) => {
   console.log(">>> check props: ", props);
   const { addNewTodo } = props;
   // addNewTodo("Phat");
+  const handleClick = () => {
+    alert("Click me");
+  };
+
+  const handleOnChange = (name) => {
+    console.log(">>> handleOnChange", name); // lay gia tri tu o input
+  };
   return (
     <div className="todo-input">
-      <input placeholder="Activity"></input>
-      <button>Add</button>
+      <input
+        placeholder="Activity"
+        type="text"
+        onChange={(event) => handleOnChange(event.target.value)} // su kien nao xay ra voi thuc thi
+      ></input>
+      <button style={{ cursor: "pointer" }} onClick={handleClick}>
+        Add
+      </button>
     </div>
   );
 };
